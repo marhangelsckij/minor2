5301741dfc93786e498c665f25a79fccb90c0b0f
Arkhangelskii Michael <arhangelsckijm@yandex.ru>
2024-11-28 20:51:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ async function getResponce() {
                 <h5 class="card-title">${content_filter[key].title}</h5>
                 <p class="card-text">${content_filter[key].description}. Цена ${content_filter[key].price} р.</p>
                 <input type="hidden" name= "vendor_code" value=${content_filter[key].vendor_code}>
-                <p class="card-text" >!!Заказать!! <input class="w-25" type="text" value="0" name="check"></p>
+                <p class="card-text" >!!Заказать!! <input class="w-25" type="number" value="0" name="check"></p>
                 </li>
                         `
             }
